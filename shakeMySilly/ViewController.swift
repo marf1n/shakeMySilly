771dd1d763de345b1154159cd7b2930c2112ffdb
Vladyslav Filippov <VadyslavFilippov@MacBook-Air-Vladyslav.local>
2016-11-23 19:19:10 +0200

    Initial Commit

---
+++ shakeMySilly/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  shakeMySilly
+//
+//  Created by Vladyslav Filippov on 23.11.16.
+//  Copyright © 2016 Vladyslav Filippov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
